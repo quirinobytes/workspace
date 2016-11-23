f2f030952ba62526f2816a84839a9ed6cc1db881
Rafael Quirino <quirinobytes@gmail.com>
2016-11-23 01:17:59 -0200

    Melhorando o script.

--- bolsa-systems/blueMoney/dataMining/mining.sh
+++ bolsa-systems/blueMoney/dataMining/mining.sh
@@ -3,114 +3,70 @@
 N=/usr/bin/node
 
 
-cd acoes/anglo-american/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/acoes/anglo-american/
 $N anglo-american.js
-cd -
 
-cd acoes/cliff_natural_resources/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/acoes/cliff_natural_resources/
 $N cliff_natural_resources.js
-cd -
 
-cd acoes/csna3/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/acoes/csna3/
 $N csna3.js
-cd -
 
-cd acoes/rio_tinto/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/acoes/rio_tinto/
 $N rio_tinto.js
-cd -
 
-
-
-cd bolsas/dji/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/bolsas/dji/
 $N dji.js 
-cd -
 
-
-
-cd bolsas/ibovespa/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/bolsas/ibovespa/
 $N ibovespa.js
-cd -
 
-
-
-cd bolsas/sp500/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/bolsas/sp500/
 $N sp500.js
-cd -
 
-
-
-cd commodities/aluminum_futures/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/commodities/aluminum_futures/
 $N aluminum.js
-cd -
 
-
-
-cd commodities/coal/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/commodities/coal/
 $N coal.js
-cd -
 
-
-
-cd commodities/copper/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/commodities/copper/
 $N copper_UK.js
 $N copper_US.js
-cd -
 
-
-cd commodities/iron/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/commodities/iron/
 $N fe62.js
 $N finance.sina.com.cn.js
 $N ore-pellets.js 
-cd -
 
-
-
-cd commodities/manganese/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/commodities/manganese/
 $N manganese.js
-cd -
 
-
-
-cd commodities/nickel/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/commodities/nickel/
 $N nickel.js
-cd -
 
-
-
-cd economics/brasil/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/economics/brasil/
 $N brasil-government-bold-yeld.js
 $N brasil-pib-growth.js
 $N fgv-consumer-confidence.js
-cd -
 
-
-
-cd economics/china/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/economics/china/
 $N business_confidence.js
 $N car_index.js
 $N gdp-growth.js
 $N housing_index.js
 $N industrial-production.js
 $N loan-to-private.js
-cd -
 
-
-
-cd economics/india/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/economics/india/
 $N gdp-growth.js
-cd -
 
-
-
-cd economics/usa/
+cd /root/workspace/bolsa-systems/blueMoney/dataMining/economics/usa/
 $N gdp-growth.js
-cd -
 
 
-cd ../csv/all
+cd /root/workspace/bolsa-systems/blueMoney/csv/all
 paste * -d, > ../consolidado.csv
 
-DATA=`date "+%d%m%Y"`
-mv ../consolidado.csv /var/www/webserver/arquivos/coleta-$DATA.csv
-
-cd ../..
+DATA=`date "+%H_%Mh-%d-%h-%Y"`
+mv /root/workspace/bolsa-systems/blueMoney/csv/consolidado.csv /var/www/webserver/arquivos/coleta-$DATA.csv
