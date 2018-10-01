34825eadc20f68294f350de0c496bbbf23611606
Rafael <quirinobytes@gmail.com>
2018-09-30 21:39:16 -0300

    OK

--- pragaNotification/pragaClient.js
+++ pragaNotification/pragaClient.js
@@ -2,7 +2,7 @@
 
 	var os = require("os")
 	const io = require("socket.io-client")
-	var socket = io.connect('http://192.168.15.15:3000')
+	var socket = io.connect('http://192.168.15.101:3000')
 
 	//buttons and inputs
 	var message = "version:" + process.argv[2]
@@ -14,10 +14,13 @@
 	//Emit message
 		socket.emit('message', {message : message })
 		socket.emit('sair', {message : "sair" })
-		
+
 		socket.on("sair", (data) => {
         console.log("Saindo: " + data.username + ": " + data.message )
-	    socket.disconnect()
+		//	    socket.disconnect()
+			    while(true){
+					socket.emit("message", { message: "while" } );
+				}
      })
 
 
