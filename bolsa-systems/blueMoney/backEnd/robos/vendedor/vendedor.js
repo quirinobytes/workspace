5ac6ec875a42866af0406a61b580ef993cb88278
Rafael <quirinobytes@gmail.com>
2018-11-25 10:26:53 -0200

    OK

--- bolsa-systems/blueMoney/backEnd/robos/vendedor/vendedor.js
+++ bolsa-systems/blueMoney/backEnd/robos/vendedor/vendedor.js
@@ -9,7 +9,7 @@ content= {};
 var debug=false;
 var REQUESTS=500;
 var filename = '/root/workspace/bolsa-systems/blueMoney/backEnd/robos/datafiles/cotacoes-v.json';
-var urlserver = 'http://192.168.15.15/vender';
+var urlserver = 'http://192.168.15.3/vender';
 
 
 
